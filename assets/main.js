4c0d281b36fa7ee63d017d2f2b99e2a886ebb367
fabrizio <faber.rvn@gmail.com>
2020-11-29 19:17:05 +0100

    Change the icon on hover and display the optional input

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,33 @@
+// DOM REFERENCE
 const createNewEventBtn = document.getElementById('add-event');
 const createNewEventForm = document.getElementById('create-new-event');
 const closeFormBtn = document.getElementById('close-icon');
+const eventTitle = document.getElementById('title');
+const eventIntialDate = document.getElementById('inital-date');
+const eventEndDateCheckbox = document.getElementById('end-date-checkbox');
+const eventEndDate = document.getElementById('end-date-option');
+const reminderCheckbox = document.getElementById('reminder-checkbox');
+const reminderDate = document.getElementById('reminder-option');
+const eventDescription = document.getElementById('description');
+const eventType = document.getElementById('type-event');
+const saveEventBtn = document.getElementById('create-event-btn');
+const cancelEventBtn = document.getElementById('cancel-event.-btn')
 
+// VARIABLES
+
+// EVENT LISTENERS
 createNewEventBtn.addEventListener('click', () => {
   createNewEventForm.classList.remove('hide');
 })
 
 closeFormBtn.addEventListener('click', () => {
   createNewEventForm.classList.add('hide');
+})
+
+eventEndDateCheckbox.addEventListener('change', () => {
+  eventEndDate.classList.toggle('hide');
+})
+
+reminderCheckbox.addEventListener('change', () => {
+  reminderDate.classList.toggle('hide');
 })
