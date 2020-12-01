06093c70e60259223ed12afeb47e9b453cd802e8
fabrizio <faber.rvn@gmail.com>
2020-12-01 20:14:43 +0100

    Update the variable for the local storage

--- assets/js/render-events.js
+++ assets/js/render-events.js
@@ -1,13 +1,12 @@
 // HTML 
-let currentDayOfTheMonth = document.querySelectorAll('.calendar__days__number')
-let pastEvents = JSON.parse(localStorage.getItem('previousEvent'))
-let currentEvents = JSON.parse(localStorage.getItem('futureEvent'))
+let currentDayOfTheMonth = document.querySelectorAll('.calendar__days__number');
+let currentEvents = JSON.parse(localStorage.getItem('allEventList'));
 let eventArray = [];
 
 // formats the date to year-month-day.
 function getFormatedDate(arr) {
     for ( let item = 0; item < arr.length; item++) {
-        eventArray.push(arr[item].initialDate.slice(0,10))
+        eventArray.push(arr[item].initialDate.slice(0,10));
     }
 }
 
@@ -21,12 +20,12 @@ function findCurrentEvents(arr) {
                     //   v          day number of the div             v                 v          day of the event        v
                     if ( parseFloat(currentDayOfTheMonth[i].textContent) === parseFloat(arr[currentEvent].substr(5).substr(3)) ) {
                         let div = document.createElement('div');
-                        div.classList.add('calendar__event')
-                        div.textContent = 'test'
-                        currentDayOfTheMonth[i].appendChild(div)
+                        div.classList.add('calendar__event');
+                        div.textContent = 'test';
+                        currentDayOfTheMonth[i].appendChild(div);
                     }
                 }
             }
-         }
+        }
     }
 }
